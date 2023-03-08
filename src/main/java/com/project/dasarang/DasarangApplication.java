27b868adb4651ba0a42171857ab20de1b821c1b2
yongbin <rhdydqls1024@naver.com>
2023-03-08 11:20:48 +0900

    :seedling: :: initial commit

---
+++ src/main/java/com/project/dasarang/DasarangApplication.java
@@ -0,0 +1,13 @@
+package com.project.dasarang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasarangApplication.class, args);
+	}
+
+}
