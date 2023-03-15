eb1cc00af04650375cb173cb0d5a2a9cc7186c74
yongbin <rhdydqls1024@naver.com>
2023-03-15 09:16:38 +0900

    :memo: :: @JpaAuditing으로 BaseTime 시간 안 들어가는 오류 수정

--- src/main/java/com/project/dasarang/DasarangApplication.java
+++ src/main/java/com/project/dasarang/DasarangApplication.java
@@ -3,11 +3,13 @@ package com.project.dasarang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 public class DasarangApplication {
 
