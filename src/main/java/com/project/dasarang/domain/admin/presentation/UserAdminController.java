26ac28016767d92399f7ff36260d640cf9e32546
yongbin1 <rhdydqls1024@naver.com>
2023-04-03 11:45:37 +0900

    :memo: :: admin controller 설명 작성

--- src/main/java/com/project/dasarang/domain/admin/presentation/UserAdminController.java
+++ src/main/java/com/project/dasarang/domain/admin/presentation/UserAdminController.java
@@ -3,6 +3,8 @@ package com.project.dasarang.domain.admin.presentation;
 import com.project.dasarang.domain.admin.service.UserAdminService;
 import com.project.dasarang.domain.user.domain.enums.UserType;
 import com.project.dasarang.domain.user.presentation.dto.response.UserListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
+@Tag(name = "유저 관리 서버")
 public class UserAdminController {
 
     private final UserAdminService userAdminService;
 
+    @Operation(summary = "유저 리스트 조회")
     @GetMapping("/user/list")
     public UserListResponse getUserList(
             @RequestParam("page") int page,
@@ -24,6 +28,7 @@ public class UserAdminController {
         return userAdminService.getUserList(page, size, UserType.ROLE_USER);
     }
 
+    @Operation(summary = "학원 리스트 조회")
     @GetMapping("/owner/list")
     public UserListResponse getOwnerList(
             @RequestParam("page") int page,
