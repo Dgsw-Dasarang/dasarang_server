2e5416147cdf63dd4a57e79be0bd228b372da0f4
yongbin1 <rhdydqls1024@naver.com>
2023-04-19 17:26:49 +0900

    :memo: :: cors header 추가

--- src/main/java/com/project/dasarang/global/config/CorsFilter.java
+++ src/main/java/com/project/dasarang/global/config/CorsFilter.java
@@ -33,7 +33,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization, key");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
