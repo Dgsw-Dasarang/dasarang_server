5770bc84225d22d4b80c3b849c66191a5754ffde
yongbin1 <rhdydqls1024@naver.com>
2023-04-19 11:02:18 +0900

    :memo: :: Swagger에 모든 요청 Header에 API Key 필수 요소 명시

--- src/main/java/com/project/dasarang/global/config/SwaggerConfiguration.java
+++ src/main/java/com/project/dasarang/global/config/SwaggerConfiguration.java
@@ -2,12 +2,14 @@ package com.project.dasarang.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -27,6 +29,19 @@ public class SwaggerConfiguration {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer").bearerFormat("JWT")))
                 .info(info);
+    }
+
+    @Bean
+    public OpenApiCustomiser openApiCustomiser() {
+        return OpenApi -> OpenApi
+                .getPaths().values().stream()
+                .flatMap(pathItem -> pathItem.readOperations().stream())
+                .forEach(operation -> operation
+                        .addParametersItem(new HeaderParameter()
+                                .name("key")
+                                .required(Boolean.TRUE)
+                                )
+                        );
     }
 
 }
