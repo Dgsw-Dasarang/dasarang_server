63acd86507f03629234e453609b63ae175a4c964
yongbin1 <rhdydqls1024@naver.com>
2023-04-27 19:20:37 +0900

    :memo: :: tossService

--- src/main/java/com/project/dasarang/global/infra/payment/service/TossService.java
+++ src/main/java/com/project/dasarang/global/infra/payment/service/TossService.java
@@ -1,9 +1,13 @@
 package com.project.dasarang.global.infra.payment.service;
 
+import com.project.dasarang.global.infra.payment.domain.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TossService {
+
+    private final PaymentRepository paymentRepository;
+
 }
