f5cdfe61a90216c358d1809bc1c3c9c26b2485dc
yongbin1 <rhdydqls1024@naver.com>
2023-06-13 15:15:30 +0900

    :memo: :: ApiProvider Class에 Log 삭제

--- src/main/java/com/project/dasarang/global/security/api/ApiProvider.java
+++ src/main/java/com/project/dasarang/global/security/api/ApiProvider.java
@@ -4,12 +4,10 @@ import com.project.dasarang.domain.api.domain.Key;
 import com.project.dasarang.domain.api.domain.repository.KeyRepository;
 import com.project.dasarang.global.security.jwt.exception.ApiKeyForbiddenException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApiProvider {
@@ -21,7 +19,6 @@ public class ApiProvider {
         byte[] encodedKeyBytes = Base64.getDecoder().decode(key);
         String encodedKeyString = new String(encodedKeyBytes);
 
-        log.info(encodedKeyString);
         Key apiKey = keyRepository.findByApiKey(encodedKeyString.substring(0, encodedKeyString.length() - 1))
                 .orElseThrow(() -> ApiKeyForbiddenException.EXCEPTION);
     }
