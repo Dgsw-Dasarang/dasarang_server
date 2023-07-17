e5bc563f48db398f810848916f78ced0a98ea054
yongbin1 <rhdydqls1024@naver.com>
2023-07-17 15:21:58 +0900

    :memo: :: 댓글 작성시 연관관계 설정

--- src/main/java/com/project/dasarang/domain/comment/service/CreateNewsCommentService.java
+++ src/main/java/com/project/dasarang/domain/comment/service/CreateNewsCommentService.java
@@ -4,6 +4,8 @@ import com.project.dasarang.domain.comment.domain.NewsComment;
 import com.project.dasarang.domain.comment.exception.CommentEmptyException;
 import com.project.dasarang.domain.comment.facade.NewsCommentFacade;
 import com.project.dasarang.domain.comment.presentation.dto.request.CreateCommentRequest;
+import com.project.dasarang.domain.news.domain.News;
+import com.project.dasarang.domain.news.facade.NewsFacade;
 import com.project.dasarang.domain.user.domain.User;
 import com.project.dasarang.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreateNewsCommentService {
 
     private final UserFacade userFacade;
+    private final NewsFacade newsFacade;
     private final NewsCommentFacade newsCommentFacade;
 
     @Transactional
-    public void execute(CreateCommentRequest request) {
+    public void execute(CreateCommentRequest request, Long newsId) {
         User user = userFacade.getCurrentUser();
 
         if (request.getComment().isEmpty())
             throw CommentEmptyException.EXCEPTION;
 
+        News news = newsFacade.findByNewsId(newsId);
+
         NewsComment newsComment = request.toNewsComment();
         newsComment.setUser(user);
+        newsComment.setNews(news);
 
         newsCommentFacade.save(newsComment);
     }
--- src/main/java/com/project/dasarang/domain/comment/service/CreatePostCommentService.java
+++ src/main/java/com/project/dasarang/domain/comment/service/CreatePostCommentService.java
@@ -4,6 +4,8 @@ import com.project.dasarang.domain.comment.domain.PostComment;
 import com.project.dasarang.domain.comment.exception.CommentEmptyException;
 import com.project.dasarang.domain.comment.facade.PostCommentFacade;
 import com.project.dasarang.domain.comment.presentation.dto.request.CreateCommentRequest;
+import com.project.dasarang.domain.post.domain.Post;
+import com.project.dasarang.domain.post.facade.PostFacade;
 import com.project.dasarang.domain.user.domain.User;
 import com.project.dasarang.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreatePostCommentService {
 
     private final UserFacade userFacade;
+    private final PostFacade postFacade;
     private final PostCommentFacade postCommentFacade;
 
     @Transactional
-    public void execute(CreateCommentRequest request) {
+    public void execute(CreateCommentRequest request, Long postId) {
         User user = userFacade.getCurrentUser();
 
         if (request.getComment().isEmpty())
             throw CommentEmptyException.EXCEPTION;
 
+        Post post = postFacade.findByPostId(postId);
+
         PostComment postComment = request.toPostComment();
         postComment.setUser(user);
+        postComment.setPost(post);
 
         postCommentFacade.save(postComment);
     }
